d9575383b45de08a380b553e028b423b4946a4e9
BeulahPreethi <beulah444@gmail.com>
2017-01-28 16:11:29 -0800

    Fully Implemented commit

--- app/src/main/java/com/preethi/ac17_workshop/MainActivity.java
+++ app/src/main/java/com/preethi/ac17_workshop/MainActivity.java
@@ -2,12 +2,34 @@ package com.preethi.ac17_workshop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button questionButton = (Button) findViewById(R.id.question_button);
+        questionButton.setOnClickListener(this);
+        Button pictureButton = (Button) findViewById(R.id.picture_button);
+        pictureButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.question_button)
+        { startActivity(new Intent(MainActivity.this,QuestionActivity.class));
+
+        }
+        else if (v.getId() == R.id.picture_button)
+        {
+            startActivity(new Intent(MainActivity.this,PictureActivity.class));
+
+        }
     }
 }
---
+++ app/src/main/java/com/preethi/ac17_workshop/QuestionActivity.java
@@ -0,0 +1,13 @@
+package com.preethi.ac17_workshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question2);
+    }
+}
