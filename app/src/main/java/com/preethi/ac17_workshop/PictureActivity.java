243cfd185b7ecc2da0b32702c4dad2faffdc726c
BeulahPreethi <beulah444@gmail.com>
2017-01-28 17:48:25 -0800

    Done with Implementing the workshop

--- app/src/main/java/com/preethi/ac17_workshop/PictureActivity.java
+++ app/src/main/java/com/preethi/ac17_workshop/PictureActivity.java
@@ -1,13 +1,32 @@
 package com.preethi.ac17_workshop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-public class PictureActivity extends AppCompatActivity {
-
+public class PictureActivity extends Activity implements OnClickListener {
+   private ImageView imageView;
+    private boolean changed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setOnClickListener(this);
+        changed = false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        changed = !changed;
+        if (changed) {
+            imageView.setImageResource(R.drawable.incorgnito);
+
+        } else {
+            imageView.setImageResource(R.drawable.phone);
+
+        }
     }
 }
--- app/src/main/java/com/preethi/ac17_workshop/QuestionActivity.java
+++ app/src/main/java/com/preethi/ac17_workshop/QuestionActivity.java
@@ -1,13 +1,39 @@
 package com.preethi.ac17_workshop;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class QuestionActivity extends Activity {
+public class QuestionActivity extends Activity  implements OnClickListener {
+
+    private EditText textField;
+    private TextView answerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question2);
+        setContentView(R.layout.activity_question);
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(this);
+
+        textField = (EditText) findViewById(R.id.text_field);
+        answerLabel = (TextView) findViewById(R.id.answer_label);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (textField.getText().toString().equals("42")) {
+            answerLabel.setText(R.string.answer_label_correct);
+            answerLabel.setTextColor(Color.rgb(0,192,0));
+        } else {
+            answerLabel.setText(R.string.answer_label_incorrec);
+            answerLabel.setTextColor(Color.rgb(255,0,0));
+        }
     }
 }
